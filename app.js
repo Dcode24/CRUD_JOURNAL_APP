ca719354ebe2d580674d098f8a06ed4a163fcd01
Daniel Ogunseye <d.ogunseye7@gmail.com>
2022-09-28 13:11:49 +0200

    Finished version of storybooks app

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
 const passport = require('passport');
 const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const connectDB = require('./config/db');
 
 // Load config
@@ -17,16 +19,47 @@ connectDB();
 
 const app = express();
 
+// Body parser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
+
 // logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// Handlebars Helpers
+const {
+  formatDate,
+  stripTags,
+  truncate,
+  editIcon,
+  select,
+} = require('./helpers/hbs');
 
 // Handlebars Template Engine
 // !Add the word .engine after exphbs
 app.engine(
   '.hbs',
   exphbs.engine({
+    helpers: {
+      formatDate,
+      stripTags,
+      truncate,
+      editIcon,
+      select,
+    },
     defaultLayout: 'main',
     extname: '.hbs',
   })
@@ -40,6 +73,9 @@ app.use(
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
+    }),
   })
 );
 
@@ -47,12 +83,19 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Set global varible
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null;
+  next();
+});
+
 // Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/', require('./routes/index'));
 app.use('/auth', require('./routes/auth'));
+app.use('/stories', require('./routes/stories'));
 
 const PORT = process.env.PORT || 8500;
 
